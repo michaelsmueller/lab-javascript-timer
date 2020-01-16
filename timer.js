46d3fa5e64d7446dbbc1cd9852c33cce49d5f594
michael <michael@miniguide.es>
2020-01-16 19:52:36 +0100

    Chrono timer initial commit

---
+++ timer.js
@@ -0,0 +1,36 @@
+/* jshint esversion: 6 */
+
+class Chrono {
+    constructor() {
+        this.timer = 0;
+    }
+    showTime () {
+        const hours = Math.floor(this.timer / 360);
+        const minutes = Math.floor( (this.timer - hours * 360) / 60);
+        const seconds = this.timer - hours * 360 - minutes * 60;
+        console.log(`${hours}h, ${minutes}m, ${seconds}s`);
+    }
+    start () {
+        const self = this;
+        self.intervalID = setInterval( function () {
+            self.timer++;
+        }, 1000);
+    }
+    // start () {
+    //     this.intervalID = setInterval( function () {
+    //         this.timer++;
+    //     }.bind(this), 1000);
+    // }
+    stop () {
+        clearInterval(this.intervalID);
+    }
+    reset () {
+        this.timer = 0;
+    }
+}
+
+let myTimer = new Chrono();
+myTimer.showTime();
+console.log(`Chrono object is named myTimer`);
+console.log(`Chrono methods are showTime(), start(), stop() and reset(). `);
+
